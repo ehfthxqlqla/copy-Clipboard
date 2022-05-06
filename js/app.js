bcf8703c4d6f4f4ef7c8b53b30fa26ecad6ffec0
ehfthxqlqla <90304739+ehfthxqlqla@users.noreply.github.com>
2022-05-06 14:47:27 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const queryString = new URLSearchParams(window.location.search)
+const copyTarget = queryString.get("copy")
+
+const newTextArea = document.createElement("textarea")
+const titleTag = document.querySelector("title")
+
+titleTag.innerText = `복사하기 - ${copyTarget}`
+
+newTextArea.style.position = "absolute"
+newTextArea.style.top = "-9999px"
+newTextArea.innerText = copyTarget
+newTextArea.select()
+newTextArea.setSelectionRange(0, 9999)
+
+if (!navigator.clipboard) {
+    const spanTag = document.querySelector(".notice")
+    spanTag.innerText = `복사됨! 이 창을 닫아주세요.`
+    document.execCommand("copy")
+    window.open('', '_self').close()
+} else {
+    navigator.clipboard.writeText(newTextArea.innerText).then(() => {
+        const spanTag = document.querySelector(".notice")
+        spanTag.innerText = `복사됨! 이 창을 닫아주세요.`
+        window.open('', '_self').close()
+    })
+}
